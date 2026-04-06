8db2f46245aca9170424cd7b00b9b5e47b3e9fda
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 08:23:39 +0530

    Update __init__.py

--- ssnbot/__init__.py
+++ ssnbot/__init__.py
@@ -9,15 +9,15 @@ load_dotenv(override=True)
 pattern = re.compile(r"^.\d+$")
 
 # vars
-APP_ID = os.environ.get("APP_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+APP_ID = os.environ.get("APP_ID", "34446649")
+API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get('OWNER_ID', ""))
-MUST_JOIN = os.environ.get("MUST_JOIN", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+OWNER_ID = int(os.environ.get('OWNER_ID', "7892805795"))
+MUST_JOIN = os.environ.get("MUST_JOIN", "-1003515041061")
 ADMINS = [
     int(user) if pattern.search(user) else user
-    for user in os.environ.get("ADMINS", "").split()
+    for user in os.environ.get("ADMINS", "7892805795").split()
 ] + [OWNER_ID]
 
 
