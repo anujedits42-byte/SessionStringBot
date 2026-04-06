4afae9c03be20d7da99df73e17db1947241bbdbf
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 12:17:11 +0530

    Update must_join.py

--- ssnbot/plugins/must_join.py
+++ ssnbot/plugins/must_join.py
@@ -20,7 +20,6 @@ async def must_join_channel(bot: Client, msg: Message):
             try:
                 await msg.reply(
                     f"You must join [this channel]({link}) to use me. After joining try again !",
-                    # disable_web_page_preview=True,
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
