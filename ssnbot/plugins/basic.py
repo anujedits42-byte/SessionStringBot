c2e1be6e95445247923d070c0f17dca87cffd6a7
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 12:05:21 +0530

    Update basic.py

--- ssnbot/plugins/basic.py
+++ ssnbot/plugins/basic.py
@@ -1,6 +1,6 @@
 from data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, Message, LinkPreviewOptions
+from pyrogram.types import InlineKeyboardMarkup, Message
 from ssnbot.db.sql import add_user, query_msg
 
 
